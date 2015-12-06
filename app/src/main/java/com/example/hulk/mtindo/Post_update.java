002e765264e88aa8698b843695a5e9eeec8152a5
martmwangi <luthermwangi2@gmail.com>
2015-12-06 23:43:01 +0300

    post update form

---
+++ app/src/main/java/com/example/hulk/mtindo/Post_update.java
@@ -0,0 +1,21 @@
+package com.example.hulk.mtindo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class Post_update extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_update);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+
+    }
+
+}
