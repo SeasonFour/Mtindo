7fdf084a8f03ae69485ed644b71abee4911874dd
martmwangi <luthermwangi2@gmail.com>
2015-12-06 06:47:40 +0300

    Created a java method to call on create for the google sign in

--- app/src/main/java/com/example/hulk/mtindo/Login.java
+++ app/src/main/java/com/example/hulk/mtindo/Login.java
@@ -4,11 +4,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();       
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
