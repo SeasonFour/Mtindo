f309d530af19a98c9c2ad7fa6a8819b378a8797d
Moses Okemwa <okemwamoses@gmail.com>
2015-12-08 16:37:54 +0300

    slider try 1

---
+++ Mtindo-master/app/src/main/java/com/example/hulk/mtindo/TransformerAdapter.java
@@ -0,0 +1,43 @@
+package com.example.hulk.mtindo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.daimajia.slider.library.SliderLayout;
+
+/**
+ * Created by moses on 12/8/15.
+ */
+
+public class TransformerAdapter extends BaseAdapter {
+    private Context mContext;
+    public TransformerAdapter(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return SliderLayout.Transformer.values().length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return SliderLayout.Transformer.values()[position].toString();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView t = (TextView) LayoutInflater.from(mContext).inflate(R.layout.item,null);
+        t.setText(getItem(position).toString());
+        return t;
+    }
+}
