bf63354a3250b2b4e845dd03fd96e21a00f1fb45
Moses Okemwa <okemwamoses@gmail.com>
2015-12-08 16:03:51 +0300

    homepage

---
+++ Mtindo-master/app/src/main/java/com/example/hulk/mtindo/homepageSlide.java
@@ -0,0 +1,15 @@
+package com.example.hulk.mtindo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by moses on 12/8/15.
+ */
+public class homepageSlide extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homepageslide);
+    }
+}
