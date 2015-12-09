9d34ec8c5c4d88606797e2581f31c78b2b781175
Moses Okemwa <okemwamoses@gmail.com>
2015-12-09 18:30:51 +0300

    slider layout

--- Mtindo-master/app/src/main/java/com/example/hulk/mtindo/homepageSlide.java
+++ Mtindo-master/app/src/main/java/com/example/hulk/mtindo/homepageSlide.java
@@ -26,13 +26,13 @@ public class homepageSlide extends AppCompatActivity implements BaseSliderView.O
         setContentView(R.layout.homepageslide);
 
         mDemoSlider = (SliderLayout)findViewById(R.id.slider);
-        HashMap<String,String> url_maps = new HashMap<String, String>();
+        HashMap<String,String> url_maps = new HashMap<>();
         url_maps.put("Hannibal", "http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
         url_maps.put("Big Bang Theory", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
         url_maps.put("House of Cards", "http://cdn3.nflximg.net/images/3093/2043093.jpg");
         url_maps.put("Game of Thrones", "http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
 
-        HashMap<String,Integer> file_maps = new HashMap<String, Integer>();
+        HashMap<String,Integer> file_maps = new HashMap<>();
         file_maps.put("Hannibal",R.drawable.hannibal);
         file_maps.put("Big Bang Theory",R.drawable.bigbang);
         file_maps.put("House of Cards",R.drawable.house);
