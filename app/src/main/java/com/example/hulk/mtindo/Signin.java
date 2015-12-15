2c879d8249e95a6ad18bca0155c15af0ad48423f
martmwangi <luthermwangi2@gmail.com>
2015-12-15 12:49:31 +0300

    Testing image convertion method

--- app/src/main/java/com/example/hulk/mtindo/Signin.java
+++ app/src/main/java/com/example/hulk/mtindo/Signin.java
@@ -1,8 +1,14 @@
 package com.example.hulk.mtindo;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class Signin extends AppCompatActivity {
@@ -17,21 +23,39 @@ public class Signin extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // Gallery selector
-//        image = (ImageView) findViewById(ImageView01);
-//        //Intent onclick add profile image button
-//        ((Button) findViewById(R.id.Button01))
-//                .setOnClickListener(new View.OnClickListener() {
-//                    public void onClick(View arg0) {
-//                        Intent intent = new Intent();
-//                        intent.setType("image/*");
-//                        intent.setAction(Intent.ACTION_GET_CONTENT);
-//                        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
-//                    }
-//                });
-//        // ATTENTION: This was auto-generated to implement the App Indexing API.
-//        // See https://g.co/AppIndexing/AndroidStudio for more information.
-//        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+//         Gallery selector
+        image = (ImageView) findViewById(R.id.imageView3);
+        //Intent onclick add profile image button
+        ((Button) findViewById(R.id.upload))
+                .setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View arg0) {
+                        Intent intent = new Intent();
+                        intent.setType("image/*");
+                        intent.setAction(Intent.ACTION_GET_CONTENT);
+                        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+                    }
+                });
+
+
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                Uri selectedImageUri = data.getData();
+                selectedImagePath = getPath(selectedImageUri);
+                System.out.println("Image Path : " + selectedImagePath);
+                image.setImageURI(selectedImageUri);
+
+            }
+        }
+    }
+
+    public String getPath(Uri uri) {
+        String[] projection = {MediaStore.Images.Media.DATA};
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
 
 
     }
