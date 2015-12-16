358c492567ebd3cef68f9f14127a9d1f7b1a0e2b
maureenwaitherero <waithereromaureen@gmail.com>
2015-12-16 09:15:26 +0300

    Implemented search action .Now add conditions to dosearch function.

---
+++ app/src/main/java/com/example/hulk/mtindo/Makeup.java
@@ -0,0 +1,13 @@
+package com.example.hulk.mtindo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Makeup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_makeup);
+    }
+}
