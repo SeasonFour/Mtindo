2cc1fc086c3a4beda6a4f0b4f57f15cf76094541
martmwangi <luthermwangi2@gmail.com>
2015-12-16 15:29:30 +0300

    Set layout manager for the recycler for stores

--- app/src/main/java/com/example/hulk/mtindo/Beauty_pros.java
+++ app/src/main/java/com/example/hulk/mtindo/Beauty_pros.java
@@ -1,13 +1,17 @@
 package com.example.hulk.mtindo;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+
+import com.firebase.client.Firebase;
 
 public class Beauty_pros extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +20,27 @@ public class Beauty_pros extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Firebase.setAndroidContext(this);
+        Firebase rootref = new Firebase("https://mtindo.firebaseio.com/stores");
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view1);
+
+
+        //Helps with performance of the recler view page
+        mRecyclerView.setHasFixedSize(true);
+
+        //This is a  linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        //Specify now the Adapter
+//        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+//        Link Adapter
+        Update_adapter update_adapter = new Update_adapter(Update.class,R.layout.update_card,ViewHolder.class,rootref);
+        mRecyclerView.setAdapter(update_adapter);
+
+
     }
 
 }
