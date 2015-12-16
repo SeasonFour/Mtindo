1af0dfd87933411668876a2c94f5fbedc8e17b8d
Moses Okemwa <okemwamoses@gmail.com>
2015-12-16 22:30:50 +0300

    added splash screen and app intro

---
+++ Mtindo-master/app/src/main/java/com/example/hulk/mtindo/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.example.hulk.mtindo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+/**
+ * Created by moses on 12/16/15.
+ */
+public class SplashScreen extends Activity {
+
+    public static int SPLASH_TIME_OUT = 10000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                findViewById(R.id.avloadingIndicatorView).setVisibility(View.VISIBLE);
+                /*after the splash screen it moves on to the MainActivity*/
+                Intent data = new Intent(SplashScreen.this, MyAppIntro.class);
+                startActivity(data);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
