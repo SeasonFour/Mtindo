61c442ba41c062a2e9e3b50d967779f28b78ebcd
martmwangi <luthermwangi2@gmail.com>
2015-12-16 15:44:31 +0300

    Set Adapter to receive data from Store

--- app/src/main/java/com/example/hulk/mtindo/Store.java
+++ app/src/main/java/com/example/hulk/mtindo/Store.java
@@ -25,4 +25,8 @@ public class Store {
         return Description;
     }
 
+    public Store(){
+
+    }
+
 }
--- app/src/main/java/com/example/hulk/mtindo/Store_Adapter.java
+++ app/src/main/java/com/example/hulk/mtindo/Store_Adapter.java
@@ -1,7 +1,31 @@
 package com.example.hulk.mtindo;
 
+import com.firebase.client.Query;
+import com.firebase.ui.FirebaseRecyclerViewAdapter;
+
 /**
  * Created by hulk on 12/16/15.
  */
-public class Store_Adapter {
+public class Store_Adapter extends FirebaseRecyclerViewAdapter<Store , ViewHolder> {
+    Class<Update> mModelClass;
+    protected int mModelLayout;
+    Class<ViewHolder> mViewHolderClass;
+    FirebaseArray mSnapshots;
+    public Store_Adapter(Class modelClass, int modelLayout, Class viewHolderClass, Query ref) {
+        super(modelClass, modelLayout, viewHolderClass, ref);
+        mModelClass = modelClass;
+        mModelLayout = modelLayout;
+        mViewHolderClass = viewHolderClass;
+        mSnapshots = new FirebaseArray(ref);
+    }
+
+
+    @Override
+    public void populateViewHolder(ViewHolder viewHolder,Store store) {
+        viewHolder.StoreName.setText(store.getStoreName());
+        viewHolder.UserName.setText(store.getUserName());
+        viewHolder.Telephone.setText(store.getTelephone());
+        viewHolder.Description1.setText(store.getDescription());
+
+    }
 }
