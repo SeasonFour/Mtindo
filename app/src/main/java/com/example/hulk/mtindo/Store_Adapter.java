3ed1df5c25aee10b4ab467cf018306fe4eb10a58
martmwangi <luthermwangi2@gmail.com>
2015-12-17 18:21:09 +0300

    String to image success

--- app/src/main/java/com/example/hulk/mtindo/Store_Adapter.java
+++ app/src/main/java/com/example/hulk/mtindo/Store_Adapter.java
@@ -1,7 +1,12 @@
 package com.example.hulk.mtindo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 import com.firebase.client.Query;
 import com.firebase.ui.FirebaseRecyclerViewAdapter;
+
+import java.io.IOException;
 
 /**
  * Created by hulk on 12/16/15.
@@ -26,9 +31,17 @@ public class Store_Adapter extends FirebaseRecyclerViewAdapter<Store , ViewHolde
         viewHolder.userName.setText(store.getuserName());
         viewHolder.telephone.setText(store.getTelephone());
         viewHolder.description1.setText(store.getDescription());
-//        viewHolder.imageViewStore.setImageBitmap(decodeFromBase64(imageEncoded));
-
+        try {
+            viewHolder.imageViewStore.setImageBitmap(decodeFromBase64(store.getImageEncoded()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
     }
+    public static Bitmap decodeFromBase64(String input) throws IOException {
+        byte[] decodedByte = com.firebase.client.utilities.Base64.decode(input);
+        return BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.length);
+    }
+
 }
