0f45b41fe6d8b604f09b0f418747634ffaccff66
Moses Okemwa <okemwamoses@gmail.com>
2015-12-17 12:46:06 +0300

    on click for card view

---
+++ Mtindo-master/app/src/main/java/com/example/hulk/mtindo/StoreCardView.java
@@ -0,0 +1,18 @@
+package com.example.hulk.mtindo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by moses on 12/17/15.
+ */
+public class StoreCardView extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.store_cardview);
+
+    }
+}
